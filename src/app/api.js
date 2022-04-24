3690bf4c7ec61ded10577f5d122c991953fe1731
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2022-04-24 11:31:44 +0200

    Use prod WS address

--- src/app/api.js
+++ src/app/api.js
@@ -1,4 +1,4 @@
-const address = process.env.REACT_APP_API || 'https://bricks-game.herokuapp.com';
-const wsAddress = process.env.REACT_APP_API || 'wss://bricks-game.herokuapp.com';
+const address = `https://${process.env.REACT_APP_API}` || 'https://bricks-game.herokuapp.com';
+const wsAddress = `wss://${process.env.REACT_APP_API}` || 'wss://bricks-game.herokuapp.com';
 
 export { address, wsAddress };
